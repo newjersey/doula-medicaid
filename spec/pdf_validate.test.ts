f301e3846e2290f39c9ce6259e169adab8c8c950
Matt Luedke <matthew@innovation.nj.gov>
2025-03-28 08:38:10 -0700

    clean up logs and comments

--- spec/pdf_validate.test.ts
+++ spec/pdf_validate.test.ts
@@ -6,17 +6,17 @@ describe('PDF Validation Tests', () => {
     const filePath = path.resolve(__dirname, './examples/horizon_valid_no_group.pdf');
     const isValid = await validatePDF(filePath);
     expect(isValid).toBe(true);
-  }, 30000);
+  }, 10000);
 
   test('valid_group.pdf should be valid', async () => {
     const filePath = path.resolve(__dirname, './examples/horizon_valid_group.pdf');
     const isValid = await validatePDF(filePath);
     expect(isValid).toBe(true);
-  }, 30000);
+  }, 10000);
 
   test('invalid_no_group_address.pdf should be invalid', async () => {
     const filePath = path.resolve(__dirname, './examples/horizon_invalid_no_group_address.pdf');
     const isValid = await validatePDF(filePath);
     expect(isValid).toBe(false);
-  }, 30000);
+  }, 10000);
 });
--- src/app/utils/pdf_validate.ts
+++ src/app/utils/pdf_validate.ts
@@ -1,39 +1,23 @@
 import { Poppler } from 'node-poppler';
 import path from 'path';
 import Tesseract from 'tesseract.js';
-import fs from 'fs';
 
 async function convertPdfToPng(pdfPath: string): Promise<string> {
-  console.log("convertPdfToPng");
-  console.log(pdfPath);
   const poppler = new Poppler();
   const outputDir = path.dirname(pdfPath);
   const options = {
-    pngFile: true, // Specify that we want PNG output
-    singleFile: false, // Produce separate image files for each page
+    firstPageToConvert: 4, // TODO: don't hardcode
+    lastPageToConvert: 4, // TODO: don't hardcode
+    pngFile: true,
+    singleFile: false,
   };
 
-  const res = await poppler.pdfToCairo(pdfPath, `${outputDir}/output`, options);
-
-  console.log(res);
-  return res;
+  return await poppler.pdfToCairo(pdfPath, `${outputDir}/output`, options);
 }
 
 async function textFromPng(imagePath: Tesseract.ImageLike): Promise<string> {
-    console.log(`Analyzing ${imagePath}...`);
-
-    // Perform OCR on the image
-    const { data: { text } } = await Tesseract.recognize(imagePath, 'eng', {
-      logger: info => console.log(info), // Optional: log progress
-    });
-
-    return text;
-
-    // console.log(`Extracted Text: \n${text}`);
-
-    // // Write the extracted text to a file
-    // fs.writeFileSync(outputTextFile, text, 'utf8');
-    // console.log(`Text written to ${outputTextFile}`);
+  const { data: { text } } = await Tesseract.recognize(imagePath, 'eng');
+  return text;
 }
 
 async function extractTextFromPDF(filePath: string): Promise<string> {
@@ -41,32 +25,24 @@ async function extractTextFromPDF(filePath: string): Promise<string> {
 
   const outputDir = path.dirname(filePath);
 
-  // Define the path to your specific PNG and the output text file
+  // TODO: don't hardcode
   const imagePath = path.resolve(outputDir, 'output-04.png');
 
-  // Run the OCR and save the result
-  const text = await textFromPng(imagePath);
-
-  return text;
+  return await textFromPng(imagePath);
 }
 
 export async function validatePDF(filePath: string): Promise<boolean> {
-
   const text = await extractTextFromPDF(filePath);
 
-  console.log(text);
-  
   const groupNamePattern = /Group Practice Name\s*(.+?)(?=\n|$)/;
   const groupAddressPattern = /Group Practice Address\s*(.+?)(?=\n|$)/;
 
   const groupNameMatch = text.match(groupNamePattern);
   const groupAddressMatch = text.match(groupAddressPattern);
 
-  // Check if the address is filled with actual content and not just placeholders like "_"
   const isAddressValid = groupAddressMatch !== null && groupAddressMatch[1].trim() !== '' && groupAddressMatch[1].trim() !== '_';
 
   if (groupNameMatch && groupNameMatch[1].trim() !== '' && groupNameMatch[1].trim() !== '_') {
-    // If there's a group name, ensure there's also a valid group address
     return isAddressValid;
   }
 
