906aef95544181b62f3d5de788d78ade1fa9c4a9
Matt Luedke <matthew@innovation.nj.gov>
2025-03-28 13:08:01 -0700

    use a temp dir to store intermediate validation data that gets deleted

--- src/app/utils/pdf_validate.ts
+++ src/app/utils/pdf_validate.ts
@@ -1,16 +1,18 @@
 import { Poppler } from 'node-poppler';
 import path from 'path';
 import Tesseract from 'tesseract.js';
+import fs from 'fs/promises';
+
+const TEMP_DIR = 'temp';
 
 async function convertPdfToPngs(pdfPath: string): Promise<string> {
   const poppler = new Poppler();
-  const outputDir = path.dirname(pdfPath);
   const options = {
     pngFile: true,
     singleFile: false,
   };
 
-  return await poppler.pdfToCairo(pdfPath, `${outputDir}/output`, options);
+  return await poppler.pdfToCairo(pdfPath, `${TEMP_DIR}/output`, options);
 }
 
 async function textFromPng(imagePath: Tesseract.ImageLike): Promise<string> {
@@ -18,13 +20,9 @@ async function textFromPng(imagePath: Tesseract.ImageLike): Promise<string> {
   return text;
 }
 
-async function extractTextFromPDF(filePath: string): Promise<string> {
-  await convertPdfToPngs(filePath);
-
-  const outputDir = path.dirname(filePath);
-
-  // TODO: don't hardcode
-  const imagePath = path.resolve(outputDir, 'output-04.png');
+async function extractTextFromPNGs(): Promise<string> {
+  // TODO: do for all contents of TEMP_DIR
+  const imagePath = path.resolve(TEMP_DIR, 'output-04.png');
 
   return await textFromPng(imagePath);
 }
@@ -46,8 +44,21 @@ const validateGroup = (text: string) => {
   return true;
 };
 
+async function cleanupTempDir() {
+  const files = await fs.readdir(TEMP_DIR);
+
+  for (const file of files) {
+    await fs.unlink(path.join(TEMP_DIR, file));
+  }
+
+  await fs.rmdir(TEMP_DIR);
+}
+
 export async function validatePDF(filePath: string): Promise<boolean> {
+  await fs.mkdir(TEMP_DIR);
   await convertPdfToPngs(filePath);
-  const text = await extractTextFromPDF(filePath);
-  return validateGroup(text);
+  const text = await extractTextFromPNGs();
+  const result = validateGroup(text);
+  await cleanupTempDir();
+  return result;
 }
