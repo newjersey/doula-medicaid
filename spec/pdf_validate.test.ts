912f9202b40a4c0019e9f3b568880777feda6c20
Matt Luedke <matthew@innovation.nj.gov>
2025-03-28 06:10:50 -0700

    path names in test

--- spec/pdf_validate.test.ts
+++ spec/pdf_validate.test.ts
@@ -3,19 +3,19 @@ import path from 'path';
 
 describe('PDF Validation Tests', () => {
   test('valid_no_group.pdf should be valid', async () => {
-    const filePath = path.resolve(__dirname, 'path/to/valid_no_group.pdf');
+    const filePath = path.resolve(__dirname, './examples/horizon_valid_no_group.pdf');
     const isValid = await validatePDF(filePath);
     expect(isValid).toBe(true);
   });
 
   test('valid_group.pdf should be valid', async () => {
-    const filePath = path.resolve(__dirname, 'path/to/valid_group.pdf');
+    const filePath = path.resolve(__dirname, './examples/horizon_valid_group.pdf');
     const isValid = await validatePDF(filePath);
     expect(isValid).toBe(true);
   });
 
-  test('invalid_no_group_address.pdf should be invalid', async () => {
-    const filePath = path.resolve(__dirname, 'path/to/invalid_no_group_address.pdf');
+  test.only('invalid_no_group_address.pdf should be invalid', async () => {
+    const filePath = path.resolve(__dirname, './examples/horizon_invalid_no_group_address.pdf');
     const isValid = await validatePDF(filePath);
     expect(isValid).toBe(false);
   });
