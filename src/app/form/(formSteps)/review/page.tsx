301c68a948c28f926675cf9bdaa3aa0c5ca1ea2a
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-05-30 17:19:09 -0400

    Pipe into existing form filling download

--- src/app/form/(formSteps)/review/page.tsx
+++ src/app/form/(formSteps)/review/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
-import React from "react";
+import { zipForms } from "@/app/utils/zip";
+import React, { useEffect, useState } from "react";
+import { fillAllForms } from "../../_utils/form";
 
 const FormStep: React.FC = () => {
-  return <div>hi</div>;
+  const [zipDownloadUrl, setZipDownloadUrl] = useState<string | null>(null);
+  useEffect(() => {
+    (async () => {
+      const filledForms = await fillAllForms({
+        firstName: window?.sessionStorage.getItem("firstName") || "",
+        ccEmail: "",
+        dob: "",
+        groupPracticeAddress: "",
+        groupPracticeName: "",
+        lastName: "",
+      });
+      const zipBlob = await zipForms(filledForms);
+      setZipDownloadUrl(URL.createObjectURL(zipBlob));
+    })();
+  }, []);
+
+  return (
+    <div>
+      <a href={zipDownloadUrl} download="filled_forms.zip">
+        Click here to download the ZIP
+      </a>
+    </div>
+  );
 };
 
 export default FormStep;
