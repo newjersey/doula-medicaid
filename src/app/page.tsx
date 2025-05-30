ad78d81bcf0fa938cc66720111722549de4271dc
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-05-30 12:52:26 -0400

    link proof of concept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,23 @@ import React from "react";
 
 const LandingPage: React.FC = () => {
   return (
-    <div>
-      <h1>Doula Common Application</h1>
-      <h2>Effortless paperwork management starts here</h2>
-      <p className="usa-intro">
-        Focus on care, not paperwork. The doula form filler streamlines your workflow, reducing
-        duplicate effort. Effortlessly convert your Fee for Service application to your preferred
-        MCO applications.
-      </p>
-      <a className="usa-button usa-button--big" href="form/personal-information">
-        Start now
-      </a>
-    </div>
+    <>
+      <div>
+        <a href="proof-of-concept">Proof of concept</a>
+      </div>
+      <div>
+        <h1>Doula Common Application</h1>
+        <h2>Effortless paperwork management starts here</h2>
+        <p className="usa-intro">
+          Focus on care, not paperwork. The doula form filler streamlines your workflow, reducing
+          duplicate effort. Effortlessly convert your Fee for Service application to your preferred
+          MCO applications.
+        </p>
+        <a className="usa-button usa-button--big" href="form/personal-information">
+          Start now
+        </a>
+      </div>
+    </>
   );
 };
 
