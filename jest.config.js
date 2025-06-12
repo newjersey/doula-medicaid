851a26eb4d114b962a39284904b61fd47266a33c
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-06-12 17:04:49 -0400

    [#24] Add first test that renders and tests a react component
    Configure and add a first test that renders and tests a react component.
    
    The configuration for this was not fun. I've added what I remember to [New App Technical Recommendations](https://docs.google.com/document/d/1Hzq_lmfyMRmKitLK_dgjiljmNbv-9kVnhiiq-6P1YcU/edit?tab=t.0).
    
    Changes include:
    - Installing RTL, and configuring `jest-dom` to always be imported
    - Using the `jsdom` test environment, instead of the default node environment
    - Extending the tsconfig specifically for tests, to use the `react-jsx` jsx compiler. This is an extension only for tests because NextJS insists on using the `preserve` jsx compiler option, and will automatically override any changes to that option when running next lint.
    
    Steps to check if this is working
    - Run `npm run test`
    - Run `sh .husky/pre-commit`
    - Run the actual application `npm run dev`
    Everything should run fine with no jsx errors.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
   transform: {
     "^.+\\.(ts|tsx)$": [
       "ts-jest",
       {
-        tsconfig: "tsconfig.json",
+        tsconfig: "tsconfig.test.json",
       },
     ],
   },
