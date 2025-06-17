afe8c297e4589b9f32cd6c204e864a66df5bdc35
John Chan <john@innovation.nj.gov>
2025-06-17 16:48:55 -0400

    Abstract writing to session storage into its own helper library

--- src/app/form/(formSteps)/personal-information/PersonalInformationStep.test.tsx
+++ src/app/form/(formSteps)/personal-information/PersonalInformationStep.test.tsx
@@ -2,16 +2,48 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import PersonalInformationStep from "./page";
 
+async function checkTextField(fieldName: string, expectedValue: string) {
+  const user = userEvent.setup();
+  render(<PersonalInformationStep />);
+  const inputField = screen.getByRole("textbox", {
+    name: fieldName,
+  });
+
+  await user.type(inputField, expectedValue);
+
+  expect(inputField).toHaveValue(expectedValue);
+}
+
 describe("<PersonalInformationStep />", () => {
   it("updates first name", async () => {
-    const user = userEvent.setup();
-    render(<PersonalInformationStep />);
+    checkTextField("First name", "Test first name");
+  });
 
-    const firstNameInput = screen.getByRole("textbox", {
-      name: "First name",
-    });
-    await user.type(firstNameInput, "Test first name");
+  it("updates middle name", async () => {
+    checkTextField("Middle name (optional)", "Test middle name");
+  });
 
-    expect(firstNameInput).toHaveValue("Test first name");
+  it("updates last name", async () => {
+    checkTextField("Last name", "Test last name");
+  });
+
+  it("updates street address 1", async () => {
+    checkTextField("Street address 1", "Test address 1");
+  });
+
+  it("updates street address 2", async () => {
+    checkTextField("Street address 2 (optional)", "Test address 2");
+  });
+
+  it("updates street address 2", async () => {
+    checkTextField("Street address 2 (optional)", "Test address 2");
+  });
+
+  it("updates city", async () => {
+    checkTextField("City", "Test city");
+  });
+
+  it("updates zip code", async () => {
+    checkTextField("ZIP", "12345");
   });
 });
