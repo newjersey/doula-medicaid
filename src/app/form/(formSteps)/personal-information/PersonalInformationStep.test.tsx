0fa51607f4fb0543ee739236b8ab6539f54d96fe
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-06-23 11:07:07 -0400

    Await tests and clean up session storage
    Without the await, the test is done once the promise is created, and does not actually wait for the promise to resolve.
    
    One can test this by adding a `expect(false).toBe(true);` to the end of `checkTextField`. The tests will pass anyway.

--- src/app/form/(formSteps)/personal-information/PersonalInformationStep.test.tsx
+++ src/app/form/(formSteps)/personal-information/PersonalInformationStep.test.tsx
@@ -15,31 +15,35 @@ async function checkTextField(fieldName: string, expectedValue: string) {
 }
 
 describe("<PersonalInformationStep />", () => {
+  afterEach(() => {
+    window.sessionStorage.clear();
+  });
+
   it("updates first name", async () => {
-    checkTextField("First name", "Test first name");
+    await checkTextField("First name", "Test first name");
   });
 
   it("updates middle name", async () => {
-    checkTextField("Middle name (optional)", "Test middle name");
+    await checkTextField("Middle name (optional)", "Test middle name");
   });
 
   it("updates last name", async () => {
-    checkTextField("Last name", "Test last name");
+    await checkTextField("Last name", "Test last name");
   });
 
   it("updates street address 1", async () => {
-    checkTextField("Street address 1", "Test address 1");
+    await checkTextField("Street address 1", "Test address 1");
   });
 
   it("updates street address 2", async () => {
-    checkTextField("Street address 2 (optional)", "Test address 2");
+    await checkTextField("Street address 2 (optional)", "Test address 2");
   });
 
   it("updates city", async () => {
-    checkTextField("City", "Test city");
+    await checkTextField("City", "Test city");
   });
 
   it("updates zip code", async () => {
-    checkTextField("ZIP", "12345");
+    await checkTextField("ZIP", "12345");
   });
 });
