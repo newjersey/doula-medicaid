aefd09d58470eab0c0752072b305aff60c1e9ec7
John Chan <john@innovation.nj.gov>
2025-07-10 12:06:05 -0400

    Added banner
    Update img tag to use next/image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "@newjersey/njwds/dist/css/styles.css";
+import njStateSeal from "@newjersey/njwds/dist/img/nj_state_seal.png";
 import type { Metadata } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,6 +23,46 @@ export default function RootLayout({
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       </Head>
       <body>
+        <section className="nj-banner" aria-label="Official government website">
+          <header className="nj-banner__header">
+            <div className="grid-container">
+              <div className="nj-banner__inner">
+                <div className="grid-col-auto">
+                  <Image
+                    className="nj-banner__header-seal"
+                    src={njStateSeal.src}
+                    alt="NJ flag"
+                    width={njStateSeal.width}
+                    height={njStateSeal.height}
+                  ></Image>
+                </div>
+                <div className="grid-col-fill">
+                  <a href="https://nj.gov">Official Site of the State of New Jersey</a>
+                </div>
+                <div className="grid-col-auto">
+                  <div className="text-white">
+                    <ul>
+                      <li>Governor Phil Murphy &bull; Lt. Governor Tahesha Way</li>
+                      <li>
+                        <a href="https://nj.gov/subscribe/" target="_blank">
+                          <svg
+                            className="usa-icon bottom-neg-2px margin-right-05"
+                            aria-hidden="true"
+                            focusable="false"
+                            role="img"
+                          >
+                            <use href={"/svg/sprite.svg#mail"}></use>
+                          </svg>
+                          Get Updates
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+        </section>
         <div className="usa-section">
           <div className="grid-container">
             <div className="grid-row">{children}</div>
