38f48510bc005751da354f9155c6f9ddaaa0dae7
John Chan <john@innovation.nj.gov>
2025-09-03 11:07:10 -0400

    [SHOW] 204 Refactor shared logic into DoulaForm component (#113)
    * Use DoulaForm component to abstract error summary logic
    
    Update personal details section
    
    Update Business Details page
    
    Build DoulaForm component
    
    Add DoulaForm test
    
    write new doula test page
    
    fix doulaForm component tests
    
    Update personal details section
    
    Update Business Details page
    
    Build DoulaForm component
    
    Add DoulaForm test
    
    write new doula test page
    
    fix doulaForm component tests
    
    * Add hasErrorSummary variable to pages
    
    * Rebase screening 1 page
    
    * PR comments
    
    * Refactor DoulaForm args to be optional based on mayHaveThreeOrMoreErrors

--- src/app/form/(formSteps)/business-details/1/page.tsx
+++ src/app/form/(formSteps)/business-details/1/page.tsx
@@ -1,28 +1,20 @@
 "use client";
 
 import type { BusinessDetails1Data } from "@/app/form/(formSteps)/business-details/BusinessDetailsData";
+import { DoulaForm } from "@/app/form/components/DoulaForm";
 import FormProgressButtons from "@form/(formSteps)/components/FormProgressButtons";
-import { createFormSubmitHandler } from "@form/_utils/formHandlers";
-import { useFormProgressPosition } from "@form/_utils/formProgressRouting";
 import { AddressState } from "@form/_utils/inputFields/enums";
-import {
-  Fieldset,
-  Form,
-  Label,
-  Radio,
-  RequiredMarker,
-  Select,
-  TextInput,
-} from "@trussworks/react-uswds";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Fieldset, Label, Radio, RequiredMarker, Select, TextInput } from "@trussworks/react-uswds";
 import { useForm } from "react-hook-form";
 
+const mayHaveThreeOrMoreErrors = false;
 const BusinessDetails1 = () => {
-  const [dataHasLoaded, setDataHasLoaded] = useState<boolean>(false);
-  const router = useRouter();
-  const formProgressPosition = useFormProgressPosition();
-  const { register, handleSubmit, watch } = useForm<BusinessDetails1Data>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<BusinessDetails1Data>({
     defaultValues: {
       hasSameBusinessAddress: "",
       businessStreetAddress1: "",
@@ -31,131 +23,124 @@ const BusinessDetails1 = () => {
       businessState: "NJ",
       businessZip: "",
     },
+    shouldFocusError: !mayHaveThreeOrMoreErrors,
   });
 
   const hasSameBusinessAddress = watch("hasSameBusinessAddress");
 
-  const onSubmit = createFormSubmitHandler<BusinessDetails1Data>(router, formProgressPosition);
-
-  useEffect(() => {
-    setDataHasLoaded(true);
-  }, []);
-
   return (
-    <div>
-      {dataHasLoaded && (
-        <Form onSubmit={handleSubmit(onSubmit)} className="maxw-full">
-          <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
-            <div className="desktop:grid-col-8">
-              <h2 className="font-heading-md">Business address</h2>
-              <p className="usa-hint">
-                This is the physical location where your business operates.
-              </p>
-              <Fieldset
-                legend={
-                  <div className="usa-label">
-                    <p className="font-ui-xs text-normal">
-                      Is your business address the same as your residential and billing address?
-                    </p>
-                    <p className="font-ui-xs text-normal">
-                      Select one <RequiredMarker />
-                    </p>
-                  </div>
-                }
-                legendStyle="large"
-              >
-                <Radio
-                  id="sameBusinessAddressYes"
-                  label="Yes"
-                  value="true"
-                  {...register("hasSameBusinessAddress")}
+    <DoulaForm<BusinessDetails1Data>
+      errors={errors}
+      handleSubmit={handleSubmit}
+      mayHaveThreeOrMoreErrors={mayHaveThreeOrMoreErrors}
+    >
+      <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
+        <div className="desktop:grid-col-8">
+          <h2 className="font-heading-md margin-top-5">Business address</h2>
+          <p className="usa-hint">This is the physical location where your business operates.</p>
+          <Fieldset
+            legend={
+              <div className="usa-label">
+                <p className="font-ui-xs text-normal">
+                  Is your business address the same as your residential and billing address?
+                </p>
+                <p className="font-ui-xs text-normal">
+                  Select one <RequiredMarker />
+                </p>
+              </div>
+            }
+            legendStyle="large"
+          >
+            <Radio
+              id="sameBusinessAddressYes"
+              label="Yes"
+              value="true"
+              {...register("hasSameBusinessAddress")}
+            />
+            <Radio
+              id="sameBusinessAddressNo"
+              label="No"
+              value="false"
+              {...register("hasSameBusinessAddress")}
+            />
+          </Fieldset>
+        </div>
+
+        {hasSameBusinessAddress === "false" && (
+          <Fieldset legend="Business address" legendStyle="srOnly">
+            <div className="grid-row grid-gap">
+              <div className="mobile-lg:grid-col-6">
+                <Label requiredMarker htmlFor="businessStreetAddress1">
+                  Street address
+                </Label>
+                <TextInput
+                  id="businessStreetAddress1"
+                  type="text"
+                  required
+                  {...register("businessStreetAddress1")}
                 />
-                <Radio
-                  id="sameBusinessAddressNo"
-                  label="No"
-                  value="false"
-                  {...register("hasSameBusinessAddress")}
+              </div>
+              <div className="mobile-lg:grid-col-6">
+                <Label htmlFor="businessStreetAddress2">Street address line 2</Label>
+                <TextInput
+                  id="businessStreetAddress2"
+                  type="text"
+                  {...register("businessStreetAddress2")}
                 />
-              </Fieldset>
+              </div>
             </div>
+            <div className="grid-row grid-gap">
+              <div className="mobile-lg:grid-col-6">
+                <Label requiredMarker htmlFor="businessCity">
+                  City
+                </Label>
+                <TextInput
+                  className="usa-input"
+                  id="businessCity"
+                  type="text"
+                  required
+                  {...register("businessCity")}
+                />
+              </div>
+            </div>
+            <div className="grid-row grid-gap">
+              <div className="mobile-lg:grid-col-6">
+                <Label requiredMarker htmlFor="businessState">
+                  State
+                </Label>
+                <Select
+                  className="usa-select"
+                  id="businessState"
+                  required
+                  {...register("businessState")}
+                >
+                  {Object.keys(AddressState).map((state) => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))}
+                </Select>
+              </div>
 
-            {hasSameBusinessAddress === "false" && (
-              <Fieldset legend="Business address" legendStyle="srOnly">
-                <div className="grid-row grid-gap">
-                  <div className="mobile-lg:grid-col-6">
-                    <Label requiredMarker htmlFor="businessStreetAddress1">
-                      Street address
-                    </Label>
-                    <TextInput
-                      id="businessStreetAddress1"
-                      type="text"
-                      required
-                      {...register("businessStreetAddress1")}
-                    />
-                  </div>
-                  <div className="mobile-lg:grid-col-6">
-                    <Label htmlFor="businessStreetAddress2">Street address line 2</Label>
-                    <TextInput
-                      id="businessStreetAddress2"
-                      type="text"
-                      {...register("businessStreetAddress2")}
-                    />
-                  </div>
-                </div>
-                <div className="grid-row grid-gap">
-                  <div className="mobile-lg:grid-col-6">
-                    <Label requiredMarker htmlFor="businessCity">
-                      City
-                    </Label>
-                    <TextInput
-                      className="usa-input"
-                      id="businessCity"
-                      type="text"
-                      required
-                      {...register("businessCity")}
-                    />
-                  </div>
-                </div>
-                <div className="grid-row grid-gap">
-                  <div className="mobile-lg:grid-col-6">
-                    <Label requiredMarker htmlFor="businessState">
-                      State
-                    </Label>
-                    <Select
-                      className="usa-select"
-                      id="businessState"
-                      required
-                      {...register("businessState")}
-                    >
-                      {Object.keys(AddressState).map((state) => (
-                        <option key={state} value={state}>
-                          {state}
-                        </option>
-                      ))}
-                    </Select>
-                  </div>
-
-                  <div className="mobile-lg:grid-col-4">
-                    <Label requiredMarker htmlFor="businessZip">
-                      ZIP code
-                    </Label>
-                    <TextInput
-                      className="usa-input usa-input--medium"
-                      id="businessZip"
-                      type="text"
-                      pattern="[\d]{5}(-[\d]{4})?"
-                      required
-                      {...register("businessZip")}
-                    />
-                  </div>
-                </div>
-              </Fieldset>
-            )}
-          </div>
-          <FormProgressButtons />
-        </Form>
-      )}
-    </div>
+              <div className="mobile-lg:grid-col-4">
+                <Label requiredMarker htmlFor="businessZip">
+                  ZIP code
+                </Label>
+                <TextInput
+                  className="usa-input usa-input--medium"
+                  id="businessZip"
+                  type="text"
+                  pattern="[\d]{5}(-[\d]{4})?"
+                  required
+                  {...register("businessZip")}
+                />
+              </div>
+            </div>
+          </Fieldset>
+        )}
+      </div>
+      <FormProgressButtons />
+    </DoulaForm>
   );
 };
 
--- src/app/form/(formSteps)/screening/1/page.tsx
+++ src/app/form/(formSteps)/screening/1/page.tsx
@@ -2,19 +2,19 @@
 
 import SoleProprietorExplainer from "@/app/form/(formSteps)/screening/1/SoleProprietorExplainer";
 import type { Screening1Data } from "@/app/form/(formSteps)/screening/ScreeningData";
-import { createFormSubmitHandler } from "@/app/form/_utils/formHandlers";
 import { getDefaultBoolean } from "@/app/form/_utils/sessionStorage";
+import { DoulaForm } from "@/app/form/components/DoulaForm";
 import FormProgressButtons from "@form/(formSteps)/components/FormProgressButtons";
-import { useFormProgressPosition } from "@form/_utils/formProgressRouting";
-import { Fieldset, Form, Radio, RequiredMarker } from "@trussworks/react-uswds";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Fieldset, Radio, RequiredMarker } from "@trussworks/react-uswds";
 import { useForm } from "react-hook-form";
 
+const orderedInputNameToLabel = {
+  isSoleProprietor:
+    "Do you manage your business as an individual doula operating as a Sole Proprietor?",
+};
+
+const mayHaveThreeOrMoreErrors = false;
 const ScreeningStep1 = () => {
-  const [dataHasLoaded, setDataHasLoaded] = useState<boolean>(false);
-  const router = useRouter();
-  const formProgressPosition = useFormProgressPosition();
   const {
     register,
     handleSubmit,
@@ -24,93 +24,83 @@ const ScreeningStep1 = () => {
     defaultValues: { isSoleProprietor: getDefaultBoolean("isSoleProprietor") },
   });
   const isSoleProprietor = watch("isSoleProprietor");
-
-  const onSubmit = createFormSubmitHandler<Screening1Data>(router, formProgressPosition);
-
-  useEffect(() => {
-    setDataHasLoaded(true);
-  }, []);
-
   return (
-    <div>
-      {dataHasLoaded && (
-        <Form onSubmit={handleSubmit(onSubmit)} className="maxw-full" noValidate>
-          <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
-            <div className="desktop:grid-col-8">
-              <h2 className="font-heading-md">This beta site is currently for Sole Proprietors</h2>
-              <p className="usa-hint">
-                If you have an LLC or another business type, use the standard Medicaid
-                Fee-for-Service application.
-              </p>
-              <Fieldset
-                legend={
-                  <div className="usa-label">
-                    <p className="font-ui-xs text-normal">
-                      Do you manage your business as an individual doula operating as a Sole
-                      Proprietor?
-                    </p>
-                    <p className="usa-hint">
-                      Most NJ FamilyCare doulas operate as Sole Proprietor.
-                    </p>
-                    <p className="font-ui-xs text-normal">
-                      Select one <RequiredMarker />
-                    </p>
-                  </div>
-                }
-                legendStyle="large"
-              >
-                <Radio
-                  id="soleProprietorYes"
-                  label="Yes"
-                  value="true"
-                  checked={isSoleProprietor === "true"}
-                  required
-                  {...register("isSoleProprietor", { required: "This question is required." })}
-                  aria-invalid={errors.isSoleProprietor ? "true" : "false"}
-                  aria-describedby={errors.isSoleProprietor && "isSoleProprietorErrorMessage"}
-                />
-                <Radio
-                  id="soleProprietorNo"
-                  label="No"
-                  value="false"
-                  checked={isSoleProprietor === "false"}
-                  required
-                  {...register("isSoleProprietor", {
-                    required: "This question is required.",
-                    validate: (value) => value === "true",
-                  })}
-                  aria-invalid={errors.isSoleProprietor ? "true" : "false"}
-                  aria-describedby={errors.isSoleProprietor && "isSoleProprietorErrorMessage"}
-                />
-                {errors.isSoleProprietor && (
-                  <span id="isSoleProprietorErrorMessage" className="usa-error-message">
-                    {errors.isSoleProprietor?.type === "validate" ? (
-                      <span>
-                        Currently this site is only for Sole Proprietors. Please use the{" "}
-                        <a
-                          href="https://www.njmmis.com/providerEnrollment.aspx "
-                          target="_blank"
-                          rel="noopener"
-                        >
-                          standard FFS application
-                        </a>
-                        .
-                      </span>
-                    ) : (
-                      errors.isSoleProprietor.message
-                    )}
+    <DoulaForm<Screening1Data>
+      errors={errors}
+      handleSubmit={handleSubmit}
+      mayHaveThreeOrMoreErrors={mayHaveThreeOrMoreErrors}
+    >
+      <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
+        <div className="desktop:grid-col-8">
+          <h2 className="font-heading-md">This beta site is currently for Sole Proprietors</h2>
+          <p className="usa-hint">
+            If you have an LLC or another business type, use the standard Medicaid Fee-for-Service
+            application.
+          </p>
+          <Fieldset
+            legend={
+              <div className="usa-label">
+                <p className="font-ui-xs text-normal">
+                  {orderedInputNameToLabel["isSoleProprietor"]}
+                </p>
+                <p className="usa-hint">Most NJ FamilyCare doulas operate as Sole Proprietor.</p>
+                <p className="font-ui-xs text-normal">
+                  Select one <RequiredMarker />
+                </p>
+              </div>
+            }
+            legendStyle="large"
+          >
+            <Radio
+              id="soleProprietorYes"
+              label="Yes"
+              value="true"
+              checked={isSoleProprietor === "true"}
+              required
+              {...register("isSoleProprietor", { required: "This question is required." })}
+              aria-invalid={errors.isSoleProprietor ? "true" : "false"}
+              aria-describedby={errors.isSoleProprietor && "isSoleProprietorErrorMessage"}
+            />
+            <Radio
+              id="soleProprietorNo"
+              label="No"
+              value="false"
+              checked={isSoleProprietor === "false"}
+              required
+              {...register("isSoleProprietor", {
+                required: "This question is required.",
+                validate: (value) => value === "true",
+              })}
+              aria-invalid={errors.isSoleProprietor ? "true" : "false"}
+              aria-describedby={errors.isSoleProprietor && "isSoleProprietorErrorMessage"}
+            />
+            {errors.isSoleProprietor && (
+              <span id="isSoleProprietorErrorMessage" className="usa-error-message">
+                {errors.isSoleProprietor?.type === "validate" ? (
+                  <span>
+                    Currently this site is only for Sole Proprietors. Please use the{" "}
+                    <a
+                      href="https://www.njmmis.com/providerEnrollment.aspx "
+                      target="_blank"
+                      rel="noopener"
+                    >
+                      standard FFS application
+                    </a>
+                    .
                   </span>
+                ) : (
+                  errors.isSoleProprietor.message
                 )}
-              </Fieldset>
-            </div>
-            <div className="form-explainer desktop:grid-col-4">
-              <SoleProprietorExplainer />
-            </div>
-          </div>
-          <FormProgressButtons />
-        </Form>
-      )}
-    </div>
+              </span>
+            )}
+          </Fieldset>
+        </div>
+        <div className="form-explainer desktop:grid-col-4">
+          <SoleProprietorExplainer />
+        </div>
+      </div>
+      <FormProgressButtons />
+    </DoulaForm>
   );
 };
 
