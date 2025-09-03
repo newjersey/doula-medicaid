ee0067c5215ac3a4821ad583aeb0462f73872bf9
John Chan <john@innovation.nj.gov>
2025-09-03 11:07:10 -0400

    [SHOW] 204 Refactor shared logic into DoulaForm component (#113)
    * Use DoulaForm component to abstract error summary logic
    
    Update personal details section
    
    Update Business Details page
    
    Build DoulaForm component
    
    Add DoulaForm test
    
    write new doula test page
    
    fix doulaForm component tests
    
    Update personal details section
    
    Update Business Details page
    
    Build DoulaForm component
    
    Add DoulaForm test
    
    write new doula test page
    
    fix doulaForm component tests
    
    * Add hasErrorSummary variable to pages
    
    * Rebase screening 1 page
    
    * PR comments
    
    * Refactor DoulaForm args to be optional based on mayHaveThreeOrMoreErrors
    
    # Conflicts:
    #	src/app/form/(formSteps)/business-details/1/page.tsx
    #	src/app/form/(formSteps)/personal-details/1/page.tsx
    #	src/app/form/(formSteps)/personal-details/2/page.tsx
    #	src/app/form/(formSteps)/screening/1/page.tsx
    #	src/app/form/(formSteps)/training/1/page.tsx

--- src/app/form/(formSteps)/screening/1/page.tsx
+++ src/app/form/(formSteps)/screening/1/page.tsx
@@ -3,19 +3,18 @@
 import DoulaYesNoRadio from "@/app/form/(formSteps)/components/DoulaYesNoRadio";
 import SoleProprietorExplainer from "@/app/form/(formSteps)/screening/1/SoleProprietorExplainer";
 import type { Screening1Data } from "@/app/form/(formSteps)/screening/ScreeningData";
-import { createFormSubmitHandler } from "@/app/form/_utils/formHandlers";
 import { getDefaultBoolean } from "@/app/form/_utils/sessionStorage";
+import { DoulaForm } from "@/app/form/components/DoulaForm";
 import FormProgressButtons from "@form/(formSteps)/components/FormProgressButtons";
-import { useFormProgressPosition } from "@form/_utils/formProgressRouting";
-import { Form } from "@trussworks/react-uswds";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
+const orderedInputNameToLabel = {
+  isSoleProprietor:
+    "Do you manage your business as an individual doula operating as a Sole Proprietor?",
+};
+
+const mayHaveThreeOrMoreErrors = false;
 const ScreeningStep1 = () => {
-  const [dataHasLoaded, setDataHasLoaded] = useState<boolean>(false);
-  const router = useRouter();
-  const formProgressPosition = useFormProgressPosition();
   const {
     register,
     handleSubmit,
@@ -25,67 +24,55 @@ const ScreeningStep1 = () => {
     defaultValues: { isSoleProprietor: getDefaultBoolean("isSoleProprietor") },
   });
   const isSoleProprietor = watch("isSoleProprietor");
-
-  const onSubmit = createFormSubmitHandler<Screening1Data>(router, formProgressPosition);
-
-  useEffect(() => {
-    setDataHasLoaded(true);
-  }, []);
-
   return (
-    <div>
-      {dataHasLoaded && (
-        <Form onSubmit={handleSubmit(onSubmit)} className="maxw-full" noValidate>
-          <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
-            <div className="desktop:grid-col-8">
-              <h2 className="font-heading-md">This beta site is currently for Sole Proprietors</h2>
-              <p className="usa-hint">
-                If you have an LLC or another business type, use the standard Medicaid
-                Fee-for-Service application.
-              </p>
-              <DoulaYesNoRadio
-                name="isSoleProprietor"
-                value={isSoleProprietor}
-                label={
-                  <div>
-                    <p>
-                      Do you manage your business as an individual doula operating as a Sole
-                      Proprietor?
-                    </p>
-                    <p className="usa-hint">
-                      Most NJ FamilyCare doulas operate as Sole Proprietor.
-                    </p>
-                  </div>
-                }
-                required
-                invalidOption={{
-                  label: "No",
-                  message: (
-                    <span>
-                      Currently this site is only for Sole Proprietors. Please use the{" "}
-                      <a
-                        href="https://www.njmmis.com/providerEnrollment.aspx "
-                        target="_blank"
-                        rel="noopener"
-                      >
-                        standard FFS application
-                      </a>
-                      .
-                    </span>
-                  ),
-                }}
-                register={register}
-                errors={errors}
-              />
-            </div>
-            <div className="form-explainer desktop:grid-col-4">
-              <SoleProprietorExplainer />
-            </div>
-          </div>
-          <FormProgressButtons />
-        </Form>
-      )}
-    </div>
+    <DoulaForm<Screening1Data>
+      errors={errors}
+      handleSubmit={handleSubmit}
+      mayHaveThreeOrMoreErrors={mayHaveThreeOrMoreErrors}
+    >
+      <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
+        <div className="desktop:grid-col-8">
+          <h2 className="font-heading-md">This beta site is currently for Sole Proprietors</h2>
+          <p className="usa-hint">
+            If you have an LLC or another business type, use the standard Medicaid Fee-for-Service
+            application.
+          </p>
+          <DoulaYesNoRadio
+            name="isSoleProprietor"
+            value={isSoleProprietor}
+            label={
+              <div>
+                <p>{orderedInputNameToLabel["isSoleProprietor"]}</p>
+                <p className="usa-hint">Most NJ FamilyCare doulas operate as Sole Proprietor.</p>
+              </div>
+            }
+            required
+            invalidOption={{
+              label: "No",
+              message: (
+                <span>
+                  Currently this site is only for Sole Proprietors. Please use the{" "}
+                  <a
+                    href="https://www.njmmis.com/providerEnrollment.aspx "
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    standard FFS application
+                  </a>
+                  .
+                </span>
+              ),
+            }}
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <div className="form-explainer desktop:grid-col-4">
+          <SoleProprietorExplainer />
+        </div>
+      </div>
+      <FormProgressButtons />
+    </DoulaForm>
   );
 };
 
