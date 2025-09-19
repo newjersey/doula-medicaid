ba90c3973d28df770a5f4b972561606d7b3d6ba2
John Chan <john@innovation.nj.gov>
2025-09-19 13:08:32 -0400

    added opposite pdf key assertion

--- src/app/form/_utils/fillPdf/ffsIndividual/page22.test.ts
+++ src/app/form/_utils/fillPdf/ffsIndividual/page22.test.ts
@@ -31,6 +31,7 @@ describe("Page 22 - disclosure of ownership and control interest statement", () 
   describe("hasDisclosableEvent", () => {
     it("checks the No checkbox when formData.hasDisclosableEvent is false", () => {
       const pdfKey = "fd452disclosableeventno";
+      const oppositePdfKey = "fd452disclosableeventyyes";
       expectNoDuplicateTest<PdfFfsIndividualPage22>(pdfKey, testedPdfKeys);
       const pdfFields = mapFfsIndividualFields(
         generateFormData({
@@ -38,10 +39,12 @@ describe("Page 22 - disclosure of ownership and control interest statement", () 
         }),
       );
       expect(pdfFields[pdfKey]).toEqual(true);
+      expect(pdfFields[oppositePdfKey]).toEqual(false);
     });
 
     it("checks the Yes checkbox when formData.hasDisclosableEvent is true", () => {
       const pdfKey = "fd452disclosableeventyyes";
+      const oppositePdfKey = "fd452disclosableeventno";
       expectNoDuplicateTest<PdfFfsIndividualPage22>(pdfKey, testedPdfKeys);
       const pdfFields = mapFfsIndividualFields(
         generateFormData({
@@ -49,6 +52,7 @@ describe("Page 22 - disclosure of ownership and control interest statement", () 
         }),
       );
       expect(pdfFields[pdfKey]).toEqual(true);
+      expect(pdfFields[oppositePdfKey]).toEqual(false);
     });
   });
 });
