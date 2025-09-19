4009985410fc6903d018ee455bbdc280ced9be25
John Chan <john@innovation.nj.gov>
2025-09-19 14:49:29 -0400

    212 business details 4 (#156)
    * Add Business Details 4
    
    * Add test for BusinessDetails4

---
+++ src/app/form/(formSteps)/business-details/4/BusinessDetails4.test.tsx
@@ -0,0 +1,111 @@
+import BusinessDetails4 from "@/app/form/(formSteps)/business-details/4/page";
+import { RouterPathnameProvider } from "@/app/form/_utils/testUtils/RouterPathnameProvider";
+import {
+  testFillFromSessionStorage,
+  testRequiredField,
+  testSaveFieldsToSessionStorage,
+  type TestField,
+} from "@/app/form/_utils/testUtils/sharedTests";
+import { render, screen } from "@testing-library/react";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const noHasBeenExcludedFromMedicaid: TestField = {
+  name: "No",
+  sessionStorageKey: "hasBeenExcludedFromMedicaid",
+  required: true,
+  requiredErrorMessage: "This question is required",
+  role: "radio",
+  testValue: "false",
+  expectedValue: "false",
+  withinGroupName:
+    "Have you ever been excluded or suspended by OIG (Office of Inspector General) from participation in Medicare, Medicaid/NJ FamilyCare, or CHIP? Select one *",
+};
+
+const yesHasBeenExcludedFromMedicaid: TestField = {
+  name: "Yes",
+  sessionStorageKey: "hasBeenExcludedFromMedicaid",
+  required: true,
+  requiredErrorMessage: "This question is required",
+  role: "radio",
+  testValue: "true",
+  expectedValue: "true",
+  withinGroupName:
+    "Have you ever been excluded or suspended by OIG (Office of Inspector General) from participation in Medicare, Medicaid/NJ FamilyCare, or CHIP? Select one *",
+};
+
+const noHasBeenSuspendedFromMedicaid: TestField = {
+  name: "No",
+  sessionStorageKey: "hasBeenSuspendedFromMedicaid",
+  required: true,
+  requiredErrorMessage: "This question is required",
+  role: "radio",
+  testValue: "false",
+  expectedValue: "false",
+  withinGroupName:
+    "Have you ever had Medicare, Medicaid/NJ FamilyCare, or CHIP enrollment/participation suspended, denied, revoked, or terminated? Select one *",
+};
+
+const yesHasBeenSuspendedFromMedicaid: TestField = {
+  name: "Yes",
+  sessionStorageKey: "hasBeenSuspendedFromMedicaid",
+  required: true,
+  requiredErrorMessage: "This question is required",
+  role: "radio",
+  testValue: "true",
+  expectedValue: "true",
+  withinGroupName:
+    "Have you ever had Medicare, Medicaid/NJ FamilyCare, or CHIP enrollment/participation suspended, denied, revoked, or terminated? Select one *",
+};
+
+const minimalTestFields: Array<TestField> = [
+  yesHasBeenExcludedFromMedicaid,
+  yesHasBeenSuspendedFromMedicaid,
+];
+const allTestFields: Array<TestField> = [
+  noHasBeenExcludedFromMedicaid,
+  noHasBeenSuspendedFromMedicaid,
+  yesHasBeenExcludedFromMedicaid,
+  yesHasBeenSuspendedFromMedicaid,
+];
+
+describe("<BusinessDetails4 />", () => {
+  const renderWithRouter = () => {
+    const mockRouter: Partial<AppRouterInstance> = {
+      push: jest.fn(),
+      refresh: jest.fn(),
+    };
+    render(
+      <RouterPathnameProvider
+        pathname="/form/business-details/4"
+        router={mockRouter as AppRouterInstance}
+      >
+        <BusinessDetails4 />
+      </RouterPathnameProvider>,
+    );
+    return mockRouter;
+  };
+
+  it("saves fields to session storage on submit", async () => {
+    await testSaveFieldsToSessionStorage(
+      minimalTestFields,
+      minimalTestFields,
+      renderWithRouter,
+      screen,
+      "/form/finish",
+    );
+  });
+
+  it.each(minimalTestFields.filter((field) => field.required === true))(
+    "marks $sessionStorageKey as required and displays an error message if it is not filed in",
+    async (field) => {
+      await testRequiredField(field, minimalTestFields, renderWithRouter, screen);
+    },
+  );
+
+  it.each(allTestFields)(
+    "fills $sessionStorageKey from session storage when page is loaded",
+    async (field) => {
+      await testFillFromSessionStorage(field, renderWithRouter, screen);
+    },
+  );
+});
--- src/app/form/(formSteps)/business-details/4/page.tsx
+++ src/app/form/(formSteps)/business-details/4/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
 import { HorizontalDivider } from "@/app/components/HorizontalDivider";
+import { type BusinessDetails4Data } from "@/app/form/(formSteps)/business-details/BusinessDetailsData";
+import DoulaYesNoRadio from "@/app/form/(formSteps)/components/DoulaYesNoRadio";
+import { getDefaultBoolean } from "@/app/form/_utils/sessionStorage";
 import { DoulaForm } from "@/app/form/components/DoulaForm";
 import FormProgressButtons from "@form/(formSteps)/components/FormProgressButtons";
 import { useForm } from "react-hook-form";
 
 const mayHaveThreeOrMoreErrors = false;
-const FormStep = () => {
+const BusinessDetails4 = () => {
   const {
-    formState: { errors },
+    register,
     handleSubmit,
-  } = useForm<object>({
-    defaultValues: {},
+    formState: { errors },
+    watch,
+  } = useForm<BusinessDetails4Data>({
+    defaultValues: {
+      hasBeenExcludedFromMedicaid: getDefaultBoolean("hasBeenExcludedFromMedicaid"),
+      hasBeenSuspendedFromMedicaid: getDefaultBoolean("hasBeenSuspendedFromMedicaid"),
+    },
     shouldFocusError: !mayHaveThreeOrMoreErrors,
   });
+  const hasBeenExcludedFromMedicaid = watch("hasBeenExcludedFromMedicaid");
+  const hasBeenSuspendedFromMedicaid = watch("hasBeenSuspendedFromMedicaid");
 
   return (
     <DoulaForm<object>
@@ -21,10 +31,44 @@ const FormStep = () => {
       handleSubmit={handleSubmit}
       mayHaveThreeOrMoreErrors={mayHaveThreeOrMoreErrors}
     >
+      <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
+        <div className="desktop:grid-col-8">
+          <div>
+            <h2 className="font-heading-md">
+              Mark Yes if these apply to your business, otherwise mark No.
+            </h2>
+            <p className="usa-hint">
+              Most individual doulas with a Sole Proprietorship business answer “No” to these
+              questions.
+            </p>
+            <DoulaYesNoRadio
+              name="hasBeenExcludedFromMedicaid"
+              value={hasBeenExcludedFromMedicaid}
+              label="Have you ever been excluded or suspended by OIG (Office of Inspector General) from participation in Medicare, Medicaid/NJ FamilyCare, or CHIP?"
+              required
+              register={register}
+              errors={errors}
+            />
+          </div>
+        </div>
+      </div>
+      <HorizontalDivider />
+      <div className="grid-row grid-gap-3 margin-top-3 margin-bottom-5">
+        <div className="desktop:grid-col-8">
+          <DoulaYesNoRadio
+            name="hasBeenSuspendedFromMedicaid"
+            value={hasBeenSuspendedFromMedicaid}
+            label="Have you ever had Medicare, Medicaid/NJ FamilyCare, or CHIP enrollment/participation suspended, denied, revoked, or terminated?"
+            required
+            register={register}
+            errors={errors}
+          />
+        </div>
+      </div>
       <HorizontalDivider />
       <FormProgressButtons />
     </DoulaForm>
   );
 };
 
-export default FormStep;
+export default BusinessDetails4;
