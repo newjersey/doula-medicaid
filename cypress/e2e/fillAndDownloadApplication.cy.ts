493339ff71f1c98edb04456a43adecf3dabe964a
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-11-19 12:20:37 -0400

    Revert "Temporarily remove the redirect so that the root production page has a 404"
    This reverts commit ea1b835e8a239d2bb4a3eb2a92b5e9e5a8f928b9.

--- cypress/e2e/fillAndDownloadApplication.cy.ts
+++ cypress/e2e/fillAndDownloadApplication.cy.ts
@@ -55,7 +55,7 @@ const expectedFields: Partial<PdfFfsIndividual> = {
 };
 
 it("should fill and download the application", () => {
-  cy.visit("/form/welcome");
+  cy.visit("/");
   cy.url().should("eq", `${Cypress.config("baseUrl")}/form/welcome`);
   cy.contains("Start now").click();
 
--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/form/welcome",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
