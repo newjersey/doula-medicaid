c0b894a2fa1255263172fd859114d61ede8adf70
Jose Sosa <jjose.sosa9@gmail.com>
2025-11-21 16:34:54 -0500

    legal pages created, paired with josephine for legal page cypress testing, added titleName for page title tests

--- src/app/form/_utils/formProgress.ts
+++ src/app/form/_utils/formProgress.ts
@@ -46,6 +46,17 @@ export const progressBarSections: Array<Section> = [
     shouldShowProgressBar: true,
     shouldShowProgressHeadingAndRequiredMessage: true,
   },
+  ...(process.env.NEXT_PUBLIC_FLAG_LEGAL === "1"
+    ? [
+        {
+          id: "legal",
+          name: "Legal",
+          numSteps: 3,
+          shouldShowProgressBar: true,
+          shouldShowProgressHeadingAndRequiredMessage: true,
+        },
+      ]
+    : []),
   {
     id: "finish",
     name: "Finish",
